f33a288b2a79b1f615f847d2839044fd62241a04
AoiEndo <battlemaster.aoi@icloud.com>
2024-07-21 03:16:04 +0900

    ミドルウェアの中身変更

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -5,19 +5,20 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
-class TrustProxies
+class TrustProxies extends Middleware
 {
 
     protected $proxies = '*'; // すべてのプロキシを信頼する設定
     protected $headers = \Illuminate\Http\Request::HEADER_X_FORWARDED_ALL;
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        return $next($request);
-    }
+//     /**
+//      * Handle an incoming request.
+//      *
+//      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+//      */
+//     public function handle(Request $request, Closure $next): Response
+//     {
+//         return $next($request);
+//     }
 }
